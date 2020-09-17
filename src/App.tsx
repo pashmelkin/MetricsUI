ff5df41185336a8b2b0dbd78aeac2650a3d0e77c
Pavel <pashmelkin@gmail.com>
2020-09-17 10:03:22 +1000

    Add material ui (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 
 function App() {
+    const [repo, setRepo] = React.useState('');
+    const [cardId, setCard] = React.useState('');
+
+
+    const handleChangeRepo = (event: any) => {
+        setRepo(event.target.value);
+    };
+    const handleChangeCard = (event: any) => {
+        setCard(event.target.value);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className="paper">
+
+                <Typography component="h1" variant="h5">
+                    Get LTT Metics
+                </Typography>
+                <form className="form" noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="fname"
+                                name="Card"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="card"
+                                label="Card Identifier"
+                                autoFocus
+                                onChange={handleChangeCard}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Select
+                                labelId="repo-select-label"
+                                id="repo-select"
+                                value={repo}
+                                onChange={handleChangeRepo}
+                            >
+                                <MenuItem value="sme-web">sme-web</MenuItem>
+                                <MenuItem value="sme-web-bff">sme-web-bff</MenuItem>
+                                <MenuItem value="both repos">both repos</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary">
+                            Get Metrics
+                        </Button>
+                    </Grid>
+
+                </form>
+            </div>
+
+
+        </Container>
     </div>
   );
 }
